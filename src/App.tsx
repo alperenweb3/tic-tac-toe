15fb2317b589ff3681a2e521a7527783b98e41f2
Alperen Özkan <alperenweb3@gmail.com>
2024-05-20 17:01:55 +0200

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css';
 
-function App() {
+interface SquareProps {
+  value: number;
+}
+
+function Square({ value }: SquareProps) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
   return (
     <>
-      <button className="square">X</button>
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
     </>
   );
 }
-
-export default App;
