314246da9e8c58afea2e94a4848e0382cfacd3de
Alperen Özkan <alperenweb3@gmail.com>
2024-05-20 18:36:57 +0200

    X onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 interface SquareProps {
-  value: number;
+  value?: number | string;
 }
 
 function Square({ value }: SquareProps) {
-  return <button className="square">{value}</button>;
+  const [squareValue, setSquareValue] = useState(value);
+  function handleClick() {
+    setSquareValue('X');
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {squareValue}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
